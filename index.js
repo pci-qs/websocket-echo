0f2a0184d2db7a79cf52eace41c93321874bf3d4
pci-qs <cpak@qompass-solutions.com>
2022-01-07 21:18:34 +0300

    .env added

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const path = require('path');
 const { createServer } = require('http');
 const WebSocket = require('ws');
 
+const port = process.env.PORT || 8080
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
-server.listen(8080, function () {
-  console.log('Listening on http://localhost:8080');
+const wss = new WebSocket.Server({ server });
+wss.on('connection', function (ws) {
+  console.log('Client connected');
+  ws.on('message', function message(data, isBinary) {
+    console.log('received: %s', data);
+    ws.send(data, { binary: isBinary });
+  });
+
+  ws.send('Connected');
+  ws.on('close', function () {
+    console.log('Client disconnected');
+  });
 });
 
-// const wss = new WebSocket.Server({ server });
-// wss.on('connection', function (ws) {
-//   console.log('Client connected');
-//   ws.on('message', function message(data, isBinary) {
-//     console.log('received: %s', data);
-//     ws.send(data, { binary: isBinary });
-//   });
-
-//   ws.send('Connected');
-//   ws.on('close', function () {
-//     console.log('Client disconnected');
-//   });
-// });
+server.listen(port, function () {
+    console.log('Listening on http://localhost:8080');
+  });
